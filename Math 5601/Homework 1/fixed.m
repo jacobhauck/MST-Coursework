dcde64d4b6989fde46b677881a32f1094a4b954c
Jacob Hauck <hauckjp@gmail.com>
2023-09-06 15:05:25 -0500

    Corrected ignores

--- Math 5601/Homework 1/fixed.m
+++ Math 5601/Homework 1/fixed.m
@@ -4,14 +4,14 @@ function result = fixed(g, x0, epsilon, epsilon_g, max_it)
     for k = 0:max_it
         xk = x_next;
         x_next = g(xk);
-        error = abs(x_next - xk);
+        cauchy_error = abs(x_next - xk);
 
         fprintf(...
             "k = %d, x_k = %.5g, Cauchy error = %.5g, f(x_k) = %.5g\n",...
-            k, xk, error, x_next...
+            k, xk, cauchy_error, x_next...
         )
 
-        if error < epsilon || abs(x_next) < epsilon_g
+        if cauchy_error < epsilon || abs(x_next) < epsilon_g
             break;
         end
     end
