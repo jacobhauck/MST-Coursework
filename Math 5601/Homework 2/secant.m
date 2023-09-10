c83e2d98a64b3df12a3163eb32d7cde0a1568414
Hauck <jphqkc@umsystem.edu>
2023-09-10 15:32:00 -0500

    Move calculation of x_next.

--- Math 5601/Homework 2/secant.m
+++ Math 5601/Homework 2/secant.m
@@ -10,7 +10,9 @@ function result = secant(f, x0, x1, epsilon, epsilon_f, max_it)
         f_km1 = f_k;
         f_k = f(x_k);
 
+        x_next = x_k - f_k * (x_k - x_km1) / (f_k - f_km1);
         cauchy_error = abs(x_k - x_km1);
+        
         fprintf( ...
             ['k = %d, x_{k-1} = %.05g, x_k = %.05g, Cauchy error = %.05g, ' ...
             'f(x_k) = %.05g\n'], ...
@@ -20,8 +22,6 @@ function result = secant(f, x0, x1, epsilon, epsilon_f, max_it)
         if cauchy_error < epsilon || abs(f_k) < epsilon_f
             break;
         end
-
-        x_next = x_k - f_k * (x_k - x_km1) / (f_k - f_km1);
     end
 
     result = x_k;
