ecb768691cc3add1298c4cdadfe910f784bdadb6
Hauck <jphqkc@umsystem.edu>
2023-10-11 09:53:31 -0500

    Update code

--- Math 5601/Midterm Project/be_fixed.m
+++ Math 5601/Midterm Project/be_fixed.m
@@ -1,5 +1,5 @@
-function result = be_fixed(f, epsilon, max_it)
+function result = be_fixed(f, epsilon, max_it, log_iterations)
 
 % creates a backward Euler solver function for y' = f(t,y)
 % that maps (x_0, t, h) to the solution of x = x_0 + h * f(t,x)
-result = @(x_0, t, h) fixed(@(x) x_0 + h * f(t,x), x_0, epsilon, max_it);
+result = @(x_0, t, h) fixed(@(x) x_0 + h * f(t,x), x_0, epsilon, max_it, log_iterations);
