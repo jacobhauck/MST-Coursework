321be7287d65df56046ba78c7bf0b72e073a2074
Jacob Hauck <hauckjp@gmail.com>
2023-11-22 10:24:57 -0600

    5601 independent study, final, hw10

--- Math 5601/Independent Study Project/maxvol.py
+++ Math 5601/Independent Study Project/maxvol.py
@@ -1,25 +1,29 @@
 import numpy as np
 
 
-def maxvol(a, initial_rows, delta=1e-2, max_iter=100):
+def maxvol(a, initial_rows=None, delta=1e-2, max_iter=100):
     """
     :param a: An n x r matrix
     :param initial_rows: a set of row indices in the matrix a giving us
-    an initial nonsingular r x r submatrix
+    an initial nonsingular r x r submatrix. Chooses a random set if None
+    is given.
     :param delta: tolerance for maximal element
     """
-    r = a.shape[1]
+    n, r = a.shape
 
     # permute rows of a so that initial_rows are the upper rows
     # Since the only modifications to a we will make are swapping rows,
     # we will just store the permutation of the rows of a in the index arrays
     # (upper_rows, lower_rows), and modify these instead of modifying a
     # directly, which is slightly more efficient.
-    upper_rows = initial_rows
+    if initial_rows is None:
+        upper_rows = np.random.permutation(n)[:r]
+    else:
+        upper_rows = initial_rows
 
     # get lower rows of a
-    lower_rows = np.array(set(range(r)).difference(map(int, upper_rows)))
-
+    lower_rows = np.array(tuple(set(range(n)).difference(map(int, upper_rows))))
+    
     # get initial lower part z of a @ (a[uppper_rows])^{-1}.
     # upper part is I by construction
     z = a[lower_rows] @ np.linalg.inv(a[upper_rows])
