db51ab8c8400f848cae7d47a8d024c0ed626b0a9
Jacob Hauck <hauckjp@gmail.com>
2023-12-07 14:13:03 -0600

    Change SimpleDataset1 to SimpleDataset

--- Math 5680/Final Project/simple_dataset.py
+++ Math 5680/Final Project/simple_dataset.py
@@ -1,10 +1,23 @@
+"""
+PyTorch Dataset class for our simple dataset.
+"""
+
 import random
 from string import ascii_lowercase
-import torch
+
+import torch.utils.data
 
 
-class SimpleDataset1(torch.utils.data.Dataset):
+class SimpleDataset(torch.utils.data.Dataset):
+    """
+    PyTorch Dataset class for our simple dataset.
+    """
+
     def __init__(self, size):
+        """
+        Blocks until dataset is generated.
+        :param size: number of input-output-pairs
+        """
         self.questions = []
         self.answers = []
         for _ in range(size):
