ea7901b056ecefb7241a09f88474715f66e44184
Jacob Hauck <hauckjp@gmail.com>
2024-10-02 12:08:01 -0500

    Various things

--- Stat 6841/Homework 2/question_4a.py
+++ Stat 6841/Homework 2/question_4a.py
@@ -5,12 +5,12 @@ rate = 2
 simulation_length = 20  # Number of events to simulate
 
 n = 0
-s = [0]
+s = [0]  # N(t) can be recovered from S_n, so we can store the path using S_n
 while n < simulation_length:
     # Simulate delay until next event. Note that NumPy uses the mean of the
     # exponential distribution as the parameter, not the rate.
     t = np.random.exponential(1/rate)
-    s.append(s[-1] + t)
+    s.append(s[n] + t)
     n += 1
 
-    print(f'Event {n} occurred at time {s[-1]}')
+    print(f'Event {n} occurred at time {s[n]}')
